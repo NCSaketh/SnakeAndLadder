212951e37cf9affd82f1b6582862ecc805df16a7
NCSaketh <nc.saketh1@gmail.com>
2020-09-27 23:52:08 +0530

    adding UC2

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -8,6 +8,14 @@ public class SnakeAndLadder {
 	static String p2 = "Player2";
 	
    public static void main(String[] args) {
+	   int a = 0;
+	   while (pos1 != 100 && pos2 !=  100)
+	   {
+		   int x = (int)Math.ceil(Math.random()*6);
+		   int type = (int)Math.ceil(Math.random()*3);
+		   
+	   }
+	   
 	
      }	
 
