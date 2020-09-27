4d32e1d8bbe2c7116e0ab69d0946fbe3d3adc63c
NCSaketh <nc.saketh1@gmail.com>
2020-09-27 23:12:26 +0530

    trying

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,14 @@
 
 public class SnakeAndLadder {
+	static int count1 = 0;
+	static int count2 = 0;
+	static int pos1 = 1;
+	static int pos2 = 1;
+	static String p1 = "Player1";
+	static String p2 = "Player2";
 	
+   public static void main(String[] args) {
+	
+     }	
 
 }
